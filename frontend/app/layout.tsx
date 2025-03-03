ad1c042bc99c358cd46cfbf49655f5ea88c875f3
Divine Success <divinesuccess228@gmail.com>
2025-03-03 12:22:57 +0100

    add favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -16,6 +16,12 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Dream Mate",
   description: "Discover visionary partnerships",
+  icons:[
+    {rel:"apple-touch-icon", type:'image/png', sizes:"180x180", url:"/apple-touch-icon.png"},
+    {rel:"icon", type:'image/png', sizes:"32x32", url:"/favicon-32x32.png"},
+    {rel:"icon", type:'image/png', sizes:"16x16", url:"/favicon-16x16.png"},
+    {rel:"manifest", url:"/site.webmanifest"},
+  ],
 };
 
 export default function RootLayout({
